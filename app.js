e41b71da0c7b72a0067d2b64ca298dbdf8c628eb
Priyadharshini M <mpriyadharshinicse23@gmail.com>
2025-03-19 22:54:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var body=document.querySelector("body");
+function light_mode(){
+    body.className="";
+    body.classList.add("light");
+}
+
+function dark_mode(){
+    body.className="";
+    body.classList.add("dark")
+}
